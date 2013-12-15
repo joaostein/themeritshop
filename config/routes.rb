1bea83fdcb04ba6432816797a217d2c596e553d3
Joao Stein <joaosteing@gmail.com>
2013-12-15 01:12:51 -0800

    Add url to about and shop links on main menu

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Themeritshop::Application.routes.draw do
   ActiveAdmin.routes(self)
   root 'home#index'
 
+  get 'shop' => 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
