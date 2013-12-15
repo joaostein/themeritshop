f923f73126a42d19fc61571efc7bcfe4cc3ab8fb
Joao Stein <joaosteing@gmail.com>
2013-12-14 21:03:11 -0800

    Add all files for devise active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Themeritshop::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
