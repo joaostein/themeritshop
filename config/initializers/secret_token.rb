ffb7f4bc5949cf198bbbe6e5b09cffe28342f2a7
Joao Stein <joaosteing@gmail.com>
2013-12-14 20:30:31 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Themeritshop::Application.config.secret_key_base = '97f7f70c47c8da4ee60bfeed8da3ce93a4eca522f8477a7ea428dcd50adc8abe7aa81dca7dc2acb266c567be961e8e15365ac806341663b44b402e61c7cd869a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Themeritshop::Application.config.session_store :cookie_store, key: '_themeritshop_session'
