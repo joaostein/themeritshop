92bb1c9ce864f234d5100f5871feed6cc33303a5
Joao Stein <joaosteing@gmail.com>
2013-12-16 21:55:56 -0800

    Add share buttons to footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require_tree .
 //= require jquery.reveal
 
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
